6c1539ebc5b02527d4835b3ae3bd41943f93a464
BKukharenko <bohdankukharenko@gmail.com>
2018-12-14 20:42:47 +0200

    Created Comment Entity and it's relation with Post

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -49,15 +49,15 @@ class Post
      */
     private $isPublished;
 
-  /**
-   * @ORM\OneToMany(targetEntity="Comment", mappedBy="Post")
-   */
-    private $comments;
-
     /**
      * @ORM\ManyToMany(targetEntity="App\Entity\Category", mappedBy="post")
      */
     private $categories;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Comment", mappedBy="post")
+     */
+    private $comments;
 
   public function __construct() {
     $this->comments = new ArrayCollection();
@@ -129,18 +129,6 @@ class Post
         return $this;
     }
 
-    public function getComments(): ?Collection
-    {
-        return $this->comments;
-    }
-
-    public function setComments(?array $comments): self
-    {
-        $this->comments = $comments;
-
-        return $this;
-    }
-
     /**
      * @return Collection|Category[]
      */
@@ -164,6 +152,37 @@ class Post
         if ($this->categories->contains($category)) {
             $this->categories->removeElement($category);
             $category->removePost($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setPost($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->contains($comment)) {
+            $this->comments->removeElement($comment);
+            // set the owning side to null (unless already changed)
+            if ($comment->getPost() === $this) {
+                $comment->setPost(null);
+            }
         }
 
         return $this;
