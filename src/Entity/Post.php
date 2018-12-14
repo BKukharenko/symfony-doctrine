a25dcf98dddd3436eff346ebcfb093c7ddd5e337
BKukharenko <bohdankukharenko@gmail.com>
2018-12-14 19:58:11 +0200

    Created Post Entity and Repository

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -2,96 +2,142 @@
 
 namespace App\Entity;
 
-use Symfony\Component\Validator\Constraints as Constraints;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PostRepository")
+ */
+class Post
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $body;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updatedAt;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isPublished;
+
+    /**
+     * @ORM\Column(type="object")
+     */
+    private $category;
+
+    /**
+     * @ORM\Column(type="array", nullable=true)
+     */
+    private $comments = [];
 
 
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
-class Post {
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
 
-  /**
-   * @var int
-   */
-  protected $id;
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
 
-  /**
-   * @var \DateTime
-   * @Constraints\DateTime()
-   */
-  protected $createdAt;
+        return $this;
+    }
 
-  /**
-   * @var \DateTime
-   * @Constraints\DateTime()
-   */
-  protected $updatedAt;
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
 
-  /**
-   * @var bool
-   * @Constraints\NotNull()
-   */
-  private $isPublished;
+    public function setBody(string $body): self
+    {
+        $this->body = $body;
 
-  /**
-   * @var string
-   * @Constraints\NotBlank()
-   */
-  private $title;
+        return $this;
+    }
 
-  /**
-   * @var string
-   * @Constraints\NotBlank()
-   */
-  private $body;
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
 
-  private $category;
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
 
-  private $tags;
+        return $this;
+    }
 
-  /**
-   * @return bool
-   */
-  public function getIsPublished(): bool {
-    return $this->isPublished;
-  }
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
 
-  /**
-   * @param bool $isPublished
-   */
-  public function setIsPublished(bool $isPublished) {
-    $this->isPublished = $isPublished;
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  /**
-   * @return string
-   */
-  public function getTitle(): ?string {
-    return $this->title;
-  }
+    public function getIsPublished(): ?bool
+    {
+        return $this->isPublished;
+    }
 
-  /**
-   * @param string $title
-   */
-  public function setTitle(string $title) {
-    $this->title = $title;
+    public function setIsPublished(bool $isPublished): self
+    {
+        $this->isPublished = $isPublished;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  /**
-   * @return string
-   */
-  public function getBody(): ?string {
-    return $this->body;
-  }
+    public function getCategory()
+    {
+        return $this->category;
+    }
 
-  /**
-   * @param string $body
-   */
-  public function setBody(string $body) {
-    $this->body = $body;
+    public function setCategory($category): self
+    {
+        $this->category = $category;
 
-    return $this;
-  }
+        return $this;
+    }
+
+    public function getComments(): ?array
+    {
+        return $this->comments;
+    }
+
+    public function setComments(?array $comments): self
+    {
+        $this->comments = $comments;
+
+        return $this;
+    }
 }
